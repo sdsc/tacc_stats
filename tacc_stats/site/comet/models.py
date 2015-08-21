84af902353d711d6a72f49655771342b0238b8ea
Dmitry Mishin (SDSC) <dmishin@sentinel.sdsc.edu>
2015-08-21 11:55:33 -0700

    Multiple recent changes

--- tacc_stats/site/comet/models.py
+++ tacc_stats/site/comet/models.py
@@ -45,17 +45,18 @@ class Job(models.Model):
     Load_L2Hits = models.BigIntegerField(null=True)
     Load_LLCHits = models.BigIntegerField(null=True)
     CPU_Usage = models.FloatField(null=True)
+    MIC_Usage = models.FloatField(null=True)
 
     def __unicode__(self):
         return str(self.id)
 
     def color(self):
         if self.status == 'COMPLETED': 
-            ret_val = "#E1EDFA"
+            ret_val = "E1EDFA"
         elif self.status == 'FAILED':
-            ret_val = "#FFC4C4"
+            ret_val = "FFB2B2"
         else:
-            ret_val = "CFCFCF"
+            ret_val = "silver"
         return ret_val
 
     def sus(self):
